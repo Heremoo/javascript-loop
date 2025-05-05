892f2aed6cf10a8674c41c8bb9b97f909db7eae3
Ice <suthammakit.p@gmail.com>
2025-05-05 09:59:02 +0700

    JavaScript Loop Assignment

--- ex-6.js
+++ ex-6.js
@@ -3,5 +3,11 @@ let companyName = "TechUp Thailand";
 let reversedCompanyName = "";
 
 // Start coding here
+for (let i = 0; i < companyName.length; i++) {
+    if (i < companyName.length) {
+        reversedCompanyName = reversedCompanyName + companyName[companyName.length-1-i]
+    }
+}
 
-console.log(reversedCompanyName);
+// console.log(reversedCompanyName);
+console.log(reversedCompanyName)
